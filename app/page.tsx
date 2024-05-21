ed01161e64b4d785f589b73c310df528a3198de8
Helliton Cruz <hellitoncruz16@gmail.com>
2024-05-21 11:06:09 -0300

    removed img on about section and adjust texts

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,6 @@ import Image from 'next/image'
 
 import './styles.scss'
 
-// import profileImage from './assets/img-dev.png'
-// import reactLogo from './assets/react-logo.svg'
-// import ts from './assets/typescript-def.svg'
-// import vite from './assets/vitejs.svg'
-// import project1 from './assets/project1.svg'
-// import project2 from './assets/project2.svg'
-// import project3 from './assets/project3.svg'
-// import project4 from './assets/project4.svg'
-
 export default function Page() {
 
   const [toggle, setToggle] = useState(1)
@@ -30,30 +21,6 @@ export default function Page() {
   function openMenu() {
     menuRef?.current?.classList.toggle("menu-mobile")
   }
-  
-  const scrolled = window.scrollY
-  
-  const toggleVisible = () => {
-    if (scrolled >= 300) {
-      setVisible(true)
-      setChangeBg(true)
-    }
-    else if (scrolled < 300) {
-      setVisible(false)
-    }
-  }
-
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    }); 
-  }
-
-  useEffect(() => {
-    setInterval(function() {window.addEventListener('scroll', toggleVisible); }, 100)
-   
-  }); 
 
   return (
     <div className='page-container'>
@@ -76,19 +43,17 @@ export default function Page() {
             <p>Desenvolvedor de Software</p>
           </div>
           <div className='social-links'>
-            <a href="/">LinkedIn</a>
-            <a href="/">GitHub</a>
+            <a href="https://www.linkedin.com/in/helliton-cruz-926a56203/" target="about_blank">LinkedIn</a>
+            <a href="https://github.com/Helliton-Xavier" target="about_blank" >GitHub</a>
           </div>
         </div>
       </div>
 
       <div className='about-container' id='about-me'>
-        <Image width={800}
-      height={500} src='/assets/img-dev.png' alt="" />
         <div className='texts-about'>
           <h3>Sobre mim</h3>
           <h4>Bahia, Brasil</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id egestas dui. Nullam finibus aliquam enim quis faucibus. Aenean ac commodo dolor, nec bibendum velit.</p>
+          <p>Sou um desenvolvedor web apaixonado, com um olhar atento para detalhes e uma sede constante por aprendizado. Minha jornada no mundo da tecnologia me permitiu adquirir uma variedade de habilidades, desde o desenvolvimento front-end até o back-end. Sou movido pela curiosidade e pela busca incessante pela excelência, sempre buscando desafios que me permitam expandir meu conhecimento e criar soluções inovadoras. Minha missão é combinar criatividade e habilidades técnicas para construir produtos digitais que impactem positivamente a vida das pessoas.</p>
           <div className='about-buttons'>
             <a href="helliton-cruz-curriculo.pdf" download="helliton-cruz-curriculo.pdf">Currículo</a>
             <a href="mailto:hellitoncruz16@gmail.com">E-mail</a>
@@ -111,17 +76,19 @@ export default function Page() {
               <div className={toggle === 1 ? 'show-content' : 'tab-content'}>
                 <h4 className='job-name-txt'>Desenvolvedor de Software Junior <span className='job-period'>Fev 2023 - Atual</span></h4>
                 <p className='job-company-txt'>Quality Digital</p>
-                <p className='job-description-txt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.</p>
+                <p className='job-description-txt'>
+                  Como desenvolvedor júnior na Quality Digital, posso expandir meu conhecimento em tecnologias de e-commerce, além de solidificar minhas habilidades em VTEX, JavaScript e React. Trabalho com plataformas como FastStore e Deco, ampliando minha experiência em desenvolvimento de lojas virtuais. Além disso, tive a oportunidade de aprender e aplicar TypeScript em meus projetos, melhorando a qualidade e a manutenibilidade do código. Atualmente na Quality Digital, sou responsável por contribuir para o desenvolvimento de novas funcionalidades, resolver problemas técnicos e colaborar em equipe para garantir a entrega de projetos de alta qualidade.</p>
               </div>
               <div className={toggle === 2 ? 'show-content' : 'tab-content'}>
                 <h4 className='job-name-txt'>Estagiário em Desenvolvimento de Software <span className='job-period'>Jun 2022 - Fev 2023</span></h4>
                 <p className='job-company-txt'>Acct Global</p>
-                <p className='job-description-txt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.</p>
+                <p className='job-description-txt'>Durante meu estágio na ACCT Global, tive a oportunidade de aprimorar minhas habilidades em desenvolvimento web, especialmente com foco em tecnologias como VTEX, JavaScript e React. Colaborei ativamente no desenvolvimento e manutenção de soluções e-commerce, trabalhando em projetos de implementação, customização e otimização de lojas online. Sob a orientação de profissionais experientes, participei de equipes multidisciplinares, adquirindo conhecimento prático e técnico sobre as melhores práticas de desenvolvimento. A experiência na ACCT Global me proporcionou uma base sólida para minha carreira como desenvolvedor front-end.</p>
               </div>
               <div className={toggle === 3 ? 'show-content' : 'tab-content'}>
                 <h4 className='job-name-txt'>Freelancer <span className='job-period'>Jan 2021 - Jun 2022</span></h4>
                 <p className='job-company-txt'>Workana</p>
-                <p className='job-description-txt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum soeellicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.</p>
+                <p className='job-description-txt'>
+                  Como freelancer dev front-end na Workana, contribuo para diversos projetos, desenvolvendo interfaces de usuário atraentes e funcionais. Trabalho em colaboração com clientes para entender suas necessidades e traduzi-las em soluções digitais eficazes. Minhas responsabilidades incluíam a criação de layouts responsivos, a implementação de designs utilizando HTML, CSS e JavaScript, Wordpress e a integração de funcionalidades interativas. Garanti a entrega de projetos de alta qualidade, cumprindo prazos e superando as expectativas dos clientes.</p>
               </div>
             </div>
           </div>
@@ -133,9 +100,9 @@ export default function Page() {
         <div className='project-contents'>
           <div className='project-item'>
             <Image width={800}
-      height={500} className= "project-img" src='/assets/img-dev.png' alt="" />
-            <h4 className='project-name'>Nome do projeto</h4>
-            <p className='project-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit interdum odio eu varius.</p>
+      height={500} className= "project-img" src='/assets/project1.svg' alt="" />
+            <h4 className='project-name'>Help Dog</h4>
+            <p className='project-description'>Projeto social que tem o objetivo de ajudar cães de rua, em situação vulnerável.</p>
             <div className='project-tecs-container'>
               <p className='project-tecs'>Tecnologias usadas no projeto</p>
               <ul className='tecs-list' >
@@ -147,14 +114,14 @@ export default function Page() {
       height={500} className='logo-tec' src='/assets/vitejs.svg' alt="" />ViteJS</li>
               </ul>
             </div>
-            <a href="/project-link" className='project-link'>Visualizar</a>
+            {/* <a href="/project-link" className='project-link'>Visualizar</a> */}
           </div>
 
           <div className='project-item'>
             <Image width={800}
-      height={500} className= "project-img" src='/assets/img-dev.png' alt="" />
-            <h4 className='project-name'>Nome do projeto</h4>
-            <p className='project-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit interdum odio eu varius.</p>
+      height={500} className= "project-img" src='/assets/project2.svg' alt="" />
+            <h4 className='project-name'>NBA infos</h4>
+            <p className='project-description'>Projeto para ter as informações em tempo real do campeonato da NBA.</p>
             <div className='project-tecs-container'>
               <p className='project-tecs'>Tecnologias usadas no projeto</p>
               <ul className='tecs-list'>
@@ -166,14 +133,14 @@ export default function Page() {
       height={500} className='logo-tec' src='/assets/vitejs.svg' alt="" />ViteJS</li>
               </ul>
             </div>
-            <a href="/project-link" className='project-link'>Visualizar</a>
+            {/* <a href="/project-link" className='project-link'>Visualizar</a> */}
           </div>
 
           <div className='project-item'>
             <Image width={800}
-      height={500} className= "project-img" src='/assets/img-dev.png' alt="" />
-            <h4 className='project-name'>Nome do projeto</h4>
-            <p className='project-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit interdum odio eu varius.</p>
+      height={500} className= "project-img" src='/assets/project3.svg' alt="" />
+            <h4 className='project-name'>La pizza</h4>
+            <p className='project-description'>Site para aqueles que amam pizza, você pode pedir a que desejar.</p>
             <div className='project-tecs-container'>
               <p className='project-tecs'>Tecnologias usadas no projeto</p>
               <ul className='tecs-list'>
@@ -185,14 +152,14 @@ export default function Page() {
       height={500} className='logo-tec' src='/assets/vitejs.svg' alt="" />ViteJS</li>
               </ul>
             </div>
-            <a href="/project-link" className='project-link'>Visualizar</a>
+            {/* <a href="/project-link" className='project-link'>Visualizar</a> */}
           </div>
 
           <div className='project-item'>
             <Image width={800}
-      height={500} className= "project-img" src='/assets/img-dev.png' alt="" />
-            <h4 className='project-name'>Nome do projeto</h4>
-            <p className='project-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit interdum odio eu varius.</p>
+      height={500} className= "project-img" src='/assets/project4.svg' alt="" />
+            <h4 className='project-name'>Portal Escolar</h4>
+            <p className='project-description'>Plataforma escolar para conseguir ter informações da situação do aluno/professor.</p>
             <div className='project-tecs-container'>
               <p className='project-tecs'>Tecnologias usadas no projeto</p>
               <ul className='tecs-list'>
@@ -204,13 +171,10 @@ export default function Page() {
       height={500} className='logo-tec' src='/assets/img-dev.png' alt="" />ViteJS</li>
               </ul>
             </div>
-            <a href="/project-link" className='project-link'>Visualizar</a>
+            {/* <a href="/project-link" className='project-link'>Visualizar</a> */}
           </div>
-
-
         </div>
       </div>
-      <button id='toTop' style={{display: visible ? 'flex' : 'none'}} onClick={scrollToTop}>&#9650;</button>
       <footer>
         <div className='footer-content'><p>@ 2024 - Helliton Cruz</p></div>
       </footer>
